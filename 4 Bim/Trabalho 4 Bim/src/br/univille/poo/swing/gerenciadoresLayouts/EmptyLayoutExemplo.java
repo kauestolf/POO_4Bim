beef9f3b4d5ec7d6c8fad15ec4a064019f677a25
kauestolf <48844350+kauestolf@users.noreply.github.com>
2019-10-18 20:19:15 -0300

    Add files via upload

---
+++ 4 Bim/Trabalho 4 Bim/src/br/univille/poo/swing/gerenciadoresLayouts/EmptyLayoutExemplo.java
@@ -0,0 +1,50 @@
+package br.univille.poo.swing.gerenciadoresLayouts;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class EmptyLayoutExemplo extends JFrame{
+	
+	
+    /**
+     * FlowLayout adiciona componentes a esquerda
+     */
+    public EmptyLayoutExemplo()
+    {
+        super("Definicao de Layout Manual");
+        
+        
+        setLayout( null);
+        
+        //Cria os paineis
+        JPanel painel = criarPanel(Color.RED,"Tamanho fixo");
+        painel.setBounds(10, 10, 100, 100);
+        
+        
+        add(painel);
+    }
+    
+    
+    // Cria um painel com a cor de fundo e um label
+    private JPanel criarPanel(Color color, String texto) {
+    	JPanel panel = new JPanel();
+    	panel.setBackground(color);
+    	panel.add(new JLabel(texto));
+    	// Configura um tamanho padrao do painel
+    	panel.setPreferredSize(new Dimension(100, 50));
+    	return panel;
+    }
+    
+    public static void main(String[] args) {
+    	EmptyLayoutExemplo border = new EmptyLayoutExemplo();
+        border.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        border.setSize(400,200);
+        border.setVisible(true);
+	}
+
+}
